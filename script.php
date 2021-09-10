c283e915b5d315fc599e21c35597231830a41f5f
Daniel Arcanjo da Silva <danarcanjosilva@gmail.com>
2021-09-10 06:00:02 -0300

    comit inicial

---
+++ script.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eduardo
+ * Date: 07/06/18
+ * Time: 00:29
+ */
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+
+
+if($idade >= 6 && $idade <= 12)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+            echo "O nadador  ".$nome. " compete na categoria " .$categorias[$i];
+    }
+}
+    else if($idade >= 13 && $idade <= 18)
+{
+        for($i = 0; $i <= count($categorias); $i++)
+    {
+            if($categorias[$i] == 'adolescente')
+                     echo "O nadador ".$nome. " compete na categoria adolescente";
+                }
+            }
+    else
+    {
+        for($i = 0; $i <= count($categorias); $i++)
+            {
+                if($categorias[$i] == 'adulto')
+                    echo "O nadador ".$nome. " compete na categoria adulto";
+                }
+            }
+        
